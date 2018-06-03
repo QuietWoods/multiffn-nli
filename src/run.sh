334220df138689734a734e5791b2c3d8d89a79fb
QuietWoods <1258481281@qq.com>
2018-06-03 09:31:33 +0000

    change

---
+++ src/run.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+python3 train.py /home/project/decomp-attn/glove/vectors.txt /home/project/decomp-attn/data/atec_2.0_train.csv /home/project/decomp-attn/data/atec_2.0_dev.csv saved-model mlp 
